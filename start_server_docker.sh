a9aaafac196883cc1e1d92d1488d055f85a1ba91
Guybrush Threepwood <sailor@guybrushs-mbp.mgh.harvard.edu>
2018-01-10 21:26:05 -0500

    add 2 workers for gunicorn

--- start_server_docker.sh
+++ start_server_docker.sh
@@ -1,2 +1,2 @@
 #python app.py
-gunicorn DrugBuddyWeb:app --bind 0.0.0.0:9999 --timeout 1800 --access-logfile - --workers 1
+gunicorn DrugBuddyWeb:app --bind 0.0.0.0:9999 --timeout 1800 --access-logfile - --workers 2
